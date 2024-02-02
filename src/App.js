467686d27c22f6c8677ab649557a49bdad3293a3
shivam <sharmashivam52465@gmail.com>
2024-02-02 17:49:08 +0530

    all commit

--- src/App.js
+++ src/App.js
@@ -1,10 +1,24 @@
 import Navbar from './components/Navbar';
 import Formtext from './components/Formtext';
+import Alert from './components/alert';
+import React, {useState } from 'react';
 function App() {
+  const [alert, setAlert] = useState(null);
+  const showAlert = (message, type)=>{
+    setAlert({
+      msg: message,
+      type: type,
+    });
+    setTimeout(() => {
+      setAlert(null);
+
+    }, 1500);
+  }
   return (
     <>
       <Navbar title="TextEditer" about="About"/>
-      <Formtext heading="Enter your text"/>
+      <Alert alert={alert}/>
+      <Formtext showAlert={showAlert} heading="Enter your text"/>
     </>
   );
 }
--- src/components/Formtext.js
+++ src/components/Formtext.js
@@ -1,33 +1,56 @@
 import React,{useState} from 'react'
-
 export default function Formtext(props) {
 
   const onhandle = (event)=>{
    settext(event.target.value)
-    console.log("hyy");
   }
   const [text, settext] = useState('enter your text');
   const onhandleUp = ()=>{
     let newText = text.toUpperCase();
     settext(newText)
+    props.showAlert('Converted to Uppper case','success');
   }
   const onhandlelo = ()=>{
     let lower = text.toLowerCase();
     settext(lower);
+    props.showAlert('Converted to lower case','success');
+  }
+  const onhandclear = ()=>{
+    let clear = '';
+    settext(clear);
+    props.showAlert('Text cleared','success');
+  }
+  const onhandclearspace =()=>{
+    let newText = text.split(/ [ ]+/);
+    settext(newText.join(' '))
+    props.showAlert('Extra sapecs is removed','success');
+  }
+  const onhandclopy =()=>{
+    var text = document.getElementById('exampleFormControlTextarea1');
+    // text.select();
+    navigator.clipboard.writeText(text.value)
+    props.showAlert('Copy to clipboard','success');
   }
   return (
     <>
-      <div className="container">
+      <div className='container'>
     <div>
         <h1>{props.heading}</h1>
-    <div className="mb-3">
-    <textarea className="form-control" value={text} onChange={onhandle} id="exampleFormControlTextarea1" rows="8"></textarea>
+    <div className='mb-3'>
+    <textarea className='form-control' value={text} onChange={onhandle} id='exampleFormControlTextarea1' rows='8'></textarea>
     </div>
-    <div className="d-flex gap-2">
+    <div className='d-flex gap-2'>
     <button className='btn btn-primary' onClick={onhandleUp}>UPPER CASE</button>
     <button className='btn btn-primary'onClick={onhandlelo}>LOWER CASE</button>
+    <button className='btn btn-primary'onClick={onhandclear}>CLEAR</button>
+    <button className='btn btn-primary'onClick={onhandclopy}>COPY TEXT</button>
+    <button className='btn btn-primary'onClick={onhandclearspace}>Remove Extra Space</button>
     </div>
     </div>
+       </div>
+       <div className='container my-2'>
+        <h1>Your summary is </h1>
+        <p>Total words is {text.split(/\s+/).filter(word => word !== '').length} and total characters is {text.length}</p>
        </div>
     </>
   );
